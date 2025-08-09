b14d91430fb05ed6a17fce5b6c3069632fe5426e
Lucian Boboc <info@lucianboboc.com>
2025-08-09 11:32:34 +0200

    Added JWT functions to create and verify

---
+++ src/api/auth.test.ts
@@ -0,0 +1,67 @@
+import {describe, it, expect, beforeAll} from "vitest";
+import {checkPasswordHash, makeJWT, validateJWT} from "./auth";
+import {hashPassword} from "./auth";
+import {UnauthorizedError} from "./errors";
+
+describe("Password Hashing", () => {
+	const password1 = "correctPassword123!";
+	const password2 = "anotherPassword456!";
+	let hash1: string;
+	let hash2: string;
+
+	beforeAll(async () => {
+		hash1 = await hashPassword(password1);
+		hash2 = await hashPassword(password2);
+	});
+
+	it("should return true for the correct password1", async () => {
+		const result = await checkPasswordHash(password1, hash1);
+		expect(result).toBe(true);
+	});
+
+	it("should return false for an incorrect password", async () => {
+		const result = await checkPasswordHash("wrongpassword", hash2);
+		expect(result).toBe(false);
+	})
+
+	it("should return false when password doesn't match a different hash", async () => {
+		const result = await checkPasswordHash(password1, hash2);
+		expect(result).toBe(false);
+	});
+
+	it("should return false for an empty password", async () => {
+		const result = await checkPasswordHash("", hash1);
+		expect(result).toBe(false);
+	});
+
+	it("should return false for an invalid hash", async () => {
+		const result = await checkPasswordHash(password1, "invalidhash");
+		expect(result).toBe(false);
+	});
+});
+
+describe("JWT", () => {
+	const secret = "my$ecret$string"
+	const otherSecret = "myOther$ecret$string"
+	const userID = "123456"
+	let token: string;
+
+	beforeAll(() => {
+		token = makeJWT(userID, 3600, secret);
+	});
+
+	it("should validate a token", () => {
+		const result = validateJWT(token, secret);
+		expect(result).toBe(userID);
+	});
+
+	it("should throw an error for an invalid token string", () => {
+		expect(() => validateJWT("invalidtoken", secret))
+			.toThrow(UnauthorizedError);
+	});
+
+	it("should throw an error when the token is signed with a wrong secret", () => {
+		expect(() => validateJWT(token, otherSecret))
+			.toThrow(UnauthorizedError);
+	});
+});
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,43 @@
 import bcrypt from "bcrypt";
+import type {JwtPayload} from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import {UnauthorizedError} from "./errors";
+
+const TOKEN_ISSUER = "chirpy";
+
+type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(userID: string, expiresIn: number, secret: string) {
+	const issuedAt = Math.floor(Date.now() / 1000);
+	const expiresAt = issuedAt + expiresIn;
+	const payload: Payload = {
+		iss: TOKEN_ISSUER,
+		sub: userID,
+		exp: expiresAt,
+		iat: issuedAt,
+	}
+
+	return jwt.sign(payload, secret, {algorithm: "HS256"});
+}
+
+export function validateJWT(tokenString: string, secret: string) {
+	let payload: Payload
+	try {
+		payload = jwt.verify(tokenString, secret) as Payload;
+	} catch (error) {
+		console.log(error);
+		throw new UnauthorizedError("Invalid JWT token");
+	}
+
+	if (payload.iss !== TOKEN_ISSUER) {
+		throw new UnauthorizedError("Invalid issuer");
+	}
+	if (!payload.sub) {
+		throw new UnauthorizedError("Invalid token userID");
+	}
+
+	return payload.sub;
+}
 
 export async function hashPassword(password: string) {
 	return bcrypt.hash(password, 10);
